b0a93ba3f50bd823af59abb30ced02dbb1454d17
Thorsten Hans <thorsten.hans@gmail.com>
2023-09-05 08:05:10 +0200

    chore: Check for idioms and Rust 2021 compatibility
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! spin-contrib-http is a small crate that provides a set of helpers that will improve your development performance when
 //! building HTTP applications with [Fermyon Spin](https://developer.fermyon.com/spin/index).
-
+#![warn(rust_2018_idioms)]
+#![warn(rust_2021_compatibility)]
 #![warn(missing_docs)]
 
 /// Extensions for working with HTTP requests
